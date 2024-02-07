a09dcd3aef2ba89c8939edbfe1007c72cc90ff9b
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-02-07 20:46:39 +0100

    Added clean script

---
+++ clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -Rf ./install
+rm -Rf ./FooBar/release
+rm -Rf ./FooBar/debug
+rm -Rf ./FooBar/build
+rm -Rf ./FooBarUser/build
+rm -Rf ./FooBarUser/release
+rm -Rf ./FooBarUser/debug
