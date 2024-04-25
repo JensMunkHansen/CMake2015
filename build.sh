c0a31696250a972afdfe59583677cbd3fe744182
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-04-25 05:52:22 +0200

    ..

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-rm -Rf ./FooBar/build
-rm -Rf ./FooBarUser/build
-cmake -B./FooBar/build -H./FooBar
+#rm -Rf ./FooBar/build
+#rm -Rf ./FooBarUser/build
+rm ./FooBar/build/CMakeCache.txt
+rm ./FooBarUser/build/CMakeCache.txt
+cmake -B./FooBar/build -H./FooBar -DBUILD_SHARED_LIBS=ON
 cmake --build ./FooBar/build
 
-cmake -B./FooBarUser/build -H./FooBarUser
+cmake -B./FooBarUser/build -H./FooBarUser -DBUILD_SHARED_LIBS=ON
 cmake --build ./FooBarUser/build
